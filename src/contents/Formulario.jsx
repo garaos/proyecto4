5ef22c19e611dbcb8d664360ec5665c2c54e32eb
garaos <g.araos@gmail.com>
2022-11-24 21:46:07 -0300

    Correccion formulario contacto

--- src/contents/Formulario.jsx
+++ src/contents/Formulario.jsx
@@ -1,41 +1,46 @@
 import { useFormik } from "formik";
+import Button from 'react-bootstrap/Button';
 import { collection, addDoc } from 'firebase/firestore';
 import { db } from "../database/firebase";
 import "./formulario.css"
 
-const Formulario =() => {
-    const { handleSubmit, handleChange, values } = useFormik( {
-        initialValues: {
-          name: "",
-          email: "",
-          contactReason: ""
-        },
-        onSubmit: async function ( values ) {
-            console.log( values )
-            try {
-              const docRef = await addDoc( collection( db, "Contactos" ), values );
-              console.log( "Document written with ID: ", docRef.id );
-            } catch ( e ) {
-              console.error( "Error adding document: ", e );
-            }
-          }
-      } )
-    
-      return (
-        <form onSubmit={ handleSubmit }>
-          <label htmlFor='name'>Nombre</label>
-          <input id='name' name='name' type="text" value={ values.name } onChange={ handleChange }></input>
-    
-          <label htmlFor='email'>Correo</label>
-          <input id='email' name='email' type="email" value={ values.email } onChange={ handleChange }></input>
-    
-          <label htmlFor='contactReason'>Motivo</label>
-          <textarea id='contactReason' name='contactReason' value={ values.contactReason } onChange={ handleChange }></textarea>
-    
-          <button type='submit'>Enviar</button>
-        </form>
-      )
+const Formulario = () => {
+  const { handleSubmit, handleChange, values } = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      contactReason: ""
+    },
+    onSubmit: async function (values, { resetForm }) {
+      console.log(values)
+      resetForm({ values: "" })
+      try {
+        const docRef = await addDoc(collection(db, "Contactos"), values);
+        console.log("ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error al agregar: ", e);
       }
+    }
+  })
+
+  return (
+
+    <form onSubmit={handleSubmit}>
+      <label htmlFor='name'>Nombre</label>
+      <input id='name' name='name' type="text" value={values.name} onChange={handleChange}></input>
+
+      <label htmlFor='email'>Correo</label>
+      <input id='email' name='email' type="email" value={values.email} onChange={handleChange}></input>
+
+      <label htmlFor='contactReason'>Motivo</label>
+      <textarea id='contactReason' name='contactReason' value={values.contactReason} onChange={handleChange}></textarea>
+
+      <div className="mb-2 mt-3">
+        <Button variant="warning" type='submit' >Enviar</Button>
+      </div>
+    </form>
+  )
+}
 export default Formulario;
 
 // hacer un post al farebase, para que se almacene, voy a tener que ocupar el servicio de autenticacion de firebase**, y el de firestore, ademas cuando quiera consumir esta base de datos, lo debo hacer por medio de useeffect y useState, addDoc, añadir documento
